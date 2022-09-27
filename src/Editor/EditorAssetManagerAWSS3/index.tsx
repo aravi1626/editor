3178ec027877a2c2fb93f83873ebd088906fd0dd
Igor Moraes <igormoraesdev@gmail.com>
2022-09-27 06:03:40 -0300

    chore: add video in asset manager

--- src/Editor/EditorAssetManagerAWSS3/index.tsx
+++ src/Editor/EditorAssetManagerAWSS3/index.tsx
@@ -3,6 +3,8 @@ import styles from './styles.module.css'
 
 import { UploadFileInput } from './UploadFileInput'
 
+type Accept = 'image/*' | 'video/*'
+
 type Props = {
 	editor?: any
 	s3Options: {
@@ -17,10 +19,11 @@ export function EditorAssetManagerAWSS3({ editor, s3Options }: Props) {
 	const containerRef = useRef<HTMLDivElement | null>(null)
 	const propsRef = useRef<any>(null)
 	const [assets, setAssets] = useState<Record<string, any>[]>([])
+	const [typeAccept, setTypeAccept] = useState<Accept>('image/*')
 
 	const formatAssetUrl = (file: Record<string, any>) => `https://${s3Options.bucket}.s3.amazonaws.com/${file.Key}`
 
-	const fetchAssets = async () => {
+	const fetchAssets = async (acceptFile?: Accept) => {
 		if (!window.global) {
 			// aws-sdk requires global to exist
 			;(window as any).global = window
@@ -30,13 +33,12 @@ export function EditorAssetManagerAWSS3({ editor, s3Options }: Props) {
 		const s3 = new AWS.S3(s3Options)
 		const params = { Bucket: s3Options.bucket }
 
+		// create const to handle image and video format
+
+		const imageType = acceptFile === 'image/*' ? /.(png|jpeg|jpg|webp|svg|gif)/gim : /(mp4|webm)/gim
+
 		s3.listObjects(params, (err, data) =>
-			setAssets(
-				data.Contents?.filter((content) => /.(png|jpeg|jpg|webp|svg|gif)/gim.test(content.Key ?? '')) as Record<
-					string,
-					any
-				>[]
-			)
+			setAssets(data.Contents?.filter((content) => imageType.test(content.Key ?? '')) as Record<string, any>[])
 		)
 	}
 
@@ -55,7 +57,9 @@ export function EditorAssetManagerAWSS3({ editor, s3Options }: Props) {
 				Key: `uploads/${file.name}`,
 				Body: file,
 			}
-			s3.upload(params).promise().then(fetchAssets)
+			s3.upload(params)
+				.promise()
+				.then(fetchAssets as any)
 		})
 	}
 
@@ -73,18 +77,23 @@ export function EditorAssetManagerAWSS3({ editor, s3Options }: Props) {
 			Key: assetFile.Key,
 		}
 
-		s3.deleteObject(params).promise().then(fetchAssets)
+		s3.deleteObject(params)
+			.promise()
+			.then(fetchAssets as any)
 	}
 
 	useEffect(() => {
 		if (!editor) return
 
 		const handleCustomAsset = (props) => {
+			const accept = props.options.accept
+
+			setTypeAccept(accept)
 			props.container.appendChild(containerRef.current)
 
 			propsRef.current = props
 
-			fetchAssets()
+			fetchAssets(accept)
 		}
 
 		editor.on('asset:custom', handleCustomAsset)
@@ -94,17 +103,31 @@ export function EditorAssetManagerAWSS3({ editor, s3Options }: Props) {
 		}
 	}, [editor])
 
+	const handleVideoSource = (asset: any) => {
+		const url = `https://${s3Options.bucket}.s3.amazonaws.com/${asset.Key}`
+
+		propsRef.current.options.target.view.opts.model.set('src', url)
+	}
+
 	return (
 		<div ref={containerRef}>
 			<div className="flex" style={{ flexDirection: 'row' }}>
-				<UploadFileInput onChange={uploadFiles} />
+				<UploadFileInput typeAccept={typeAccept} onChange={uploadFiles} />
 				<div className={styles.assets_container}>
 					{assets.map((asset) => (
 						<div
 							key={asset.Key}
 							className={styles.asset}
-							onClick={() => propsRef.current.select(formatAssetUrl(asset), false)}>
-							<img src={formatAssetUrl(asset)} alt="" />
+							onClick={() =>
+								typeAccept === 'image/*'
+									? propsRef.current.select(formatAssetUrl(asset), false)
+									: handleVideoSource(asset)
+							}>
+							{typeAccept === 'image/*' ? (
+								<img src={formatAssetUrl(asset)} alt="" />
+							) : (
+								<video src={formatAssetUrl(asset)} muted />
+							)}
 							<div className={styles.info}>{asset.Key}</div>
 							<div
 								className={styles.close}
